96be937d5f89a31c8c734dc58b37d3eb8f5c962d
Shahd Mostafa <164599571+Shahd3711@users.noreply.github.com>
2025-11-21 15:22:59 +0200

    pixelgrid.h

--- pixelgrid.h
+++ pixelgrid.h
@@ -1,8 +1,9 @@
 #ifndef TERMINAL_UTILS_H
 #define TERMINAL_UTILS_H
 #include <string>
+using namespace std;
 enum colors{ black=30, red, green, yellow, blue, magenta, cyan, white, bright_black=90, bright_red, bright_green, bright_yellow, bright_blue, bright_magenta, bright_cyan, bright_white};
-void printAt(int x, int y, const std::string& text, colors colorName = white);
+void printAt(int x, int y, const string& text, colors colorName = white);
 void clearScreen();
 void sleep(int );
 #endif
